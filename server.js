f4cbea41d65ae7b89760668da0f760c86dd3cc91
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-07-31 19:42:18 +0530

    using node modules

--- server.js
+++ server.js
@@ -1,19 +1,2 @@
-const fs = require('fs');
-const path = require('path');
-
-fs.readFile('./files/text.txt', (err, data) => {
-    if (err) throw err;
-    console.log(data.toString());
-})
-fs.writeFile(path.join(__dirname, 'files', 'text.txt'), '\nHello World!', (err) => {
-    if (err) throw err;
-    console.log("Write Complete");
-    fs.appendFile(path.join(__dirname, 'files', 'text.txt'), '\nHello YOU!', (err) => {
-        if (err) throw err;
-        console.log("Append Complete");
-        fs.rename(path.join(__dirname, 'files', 'text.txt'), path.join(__dirname, 'files', 'HelloAgain.txt'), (err) => {
-            if (err) throw err;
-            console.log("Rename Complete");
-        })
-    })
-})
+const { format } = require("date-fns");
+console.log(format(new Date(), 'yyyy-MM-dd\thh:mm:ss'));
