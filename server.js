fba5abc69fc7a656979ad281978e13eca6bb5ed7
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-15 22:10:28 +0530

    Middleware

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const logger = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)?', (req, res) => {
+app.use(express.urlencoded({ extended: false }));  //Built-in middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(logger);
+
+app.get('^/$|/index.html', (req, res) => {
     // res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-app.get('/new-page(.html)?', (req, res) => {
+app.get('/new-page.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
 app.get('/hello.html', (req, res, next) => {
     console.log("Attempted to access hello.html")
     next()
