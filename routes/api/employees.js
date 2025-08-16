cca396108016497ad18930d34740659892ed5e25
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-16 21:31:58 +0530

    Routes

---
+++ routes/api/employees.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const data = {};
+data.employees = require('../../data/employees.json');
+
+router.route('/')
+    .get((req, res) => {
+        res.json(data.employees);
+    })
+    .post((req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname
+        });
+    })
+put((req, res) => {
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,45 +1,53 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const logger = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 
 app.use(express.urlencoded({ extended: false }));  //Built-in middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.use(logger);
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get('^/$|/index.html', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/new-page.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-// app.get('/*', (req, res) => {
-//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-// });
+app.use(logger);  //Custom middleware
+
+const whitelist = ['http://localhost:3500/', 'https://www.mysite.com', 'https://website.com'];  //Third party middleware
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
 app.get('/hello.html', (req, res, next) => {
     console.log("Attempted to access hello.html")
     next()
 }, (req, res) => {
     res.send("Hello");
 });
-// Chaining route handler
-const one = (req, res, next) => {
-    console.log("One");
-    next()
-}
-const two = (req, res, next) => {
-    console.log("two");
-    next()
-}
-const three = (req, res) => {
-    console.log("three");
-    res.send("Finished");
-}
-app.get("/chain.html", [one, two, three]);
+// app.use('/*', (req, res) => {
+//     res.status(404)
+//     if (req.accepts('html')) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'));
+//     } else if (req.accepts('json')) {
+//         res.json({ error: "404 Not Found!" })
+//     } else {
+//         res.type('txt').send("404 Not Found!")
+//     }
+// });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
