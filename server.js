b92303fb290a78b768cbc41524b42afa03e4887c
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-17 22:02:26 +0530

    Authentication

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 // app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
-// app.use('/subdir', require('./routes/subdir'));
+app.use('/register', require('./routes/api/register'));
+app.use('/auth', require('./routes/api/auth'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.use(logger);  //Custom middleware
