546f344ba9017341bd4bb7a29d39b90551ef055d
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-21 22:47:22 +0530

    User roles

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('./../../controllers/employeesController');
+const ROLES_LIST = require('./../../config/roles_list');
+const verifyRoles = require('./../../middleware/verifyRoles');
 
 router.route('/')
     .get(employeesController.getAllEmployees)
-    .post(employeesController.creatNewEmployee)
-    .put(employeesController.updateEmployees)
-    .delete(employeesController.deleteEmployees);
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.creatNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployees)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployees);
 
 router.route('/:id')
     .get(employeesController.getEmployee);
